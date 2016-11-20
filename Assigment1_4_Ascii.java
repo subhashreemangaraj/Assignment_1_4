b0c9aa9532d7709880e0e94958fa939d107bc04d
subhashreemangaraj <subhashree.202@gmail.com>
2016-11-20 23:21:51 +0530

    Assignment1_4

---
+++ Assigment1_4_Ascii.java
@@ -0,0 +1,17 @@
+
+public class Assigment1_4_Ascii {
+
+public static void main(String[] args) {
+	
+	int i;
+	char j;
+	
+	for ( i = 65; i <=90; i++) {
+		j = (char) i;
+		System.out.println(i +"="+j);
+	}
+	
+}
+
+
+}
